f162baa2edbe81bbc12b88d907d721ed86ba1f4e
fabilqis <fadillah.bilqis@gmail.com>
2018-09-14 18:08:04 +0700

    add heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const ticket = require('./routes/index')
 const config = require('./config/index')
-
+const PORT = process.env.PORT || 8080;
 const app = express()
 
 app.use(bodyParser.urlencoded({extended:true}))
@@ -15,4 +15,6 @@ mongoose.connect(config.url)
 app.use(cors())
 
 ticket(app)
-app.listen(8080,() => console.log('listening port 8080'))
+
+app.listen(PORT);
+console.log(`app listening on port ${PORT}`);
